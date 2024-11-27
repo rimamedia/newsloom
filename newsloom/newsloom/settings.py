7ed490d5870b0cd0e069019c0623a3badddd1a9c
ivan <tomatyss@gmail.com>
2024-11-26 19:49:34 -0500

    add homw page

--- newsloom/newsloom/settings.py
+++ newsloom/newsloom/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'newsloom.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'newsloom' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
