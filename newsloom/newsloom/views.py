4fc94097834dd4fa72bb2207078c0aecafb2895e
ivan <tomatyss@gmail.com>
2024-11-28 20:15:10 -0500

    added helthcheck

--- newsloom/newsloom/views.py
+++ newsloom/newsloom/views.py
@@ -1,5 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, "newsloom/home.html")
+
+
+def health_check(request):
+    return HttpResponse("OK")
