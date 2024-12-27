7fc92e8e43fe999ff79cb7ead62aac3d841a48e9
ivan <tomatyss@gmail.com>
2024-12-26 19:27:16 -0500

    Update static and media file paths in settings and nginx configuration

--- newsloom/newsloom/settings.py
+++ newsloom/newsloom/settings.py
@@ -154,9 +154,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = "/app/staticfiles"
+STATIC_ROOT = str(BASE_DIR / "static")
 MEDIA_URL = "/media/"
-MEDIA_ROOT = "/app/mediafiles"
+MEDIA_ROOT = str(BASE_DIR / "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
