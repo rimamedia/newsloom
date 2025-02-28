8ff972044908df13945417df1480d265935a9194
sevagol <sevagol1553@gmail.com>
2025-02-28 23:03:07 +0200

    Add origin validator

--- newsloom/newsloom/asgi.py
+++ newsloom/newsloom/asgi.py
@@ -1,27 +1,30 @@
-"""
-ASGI config for newsloom project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
-
 import django
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "newsloom.settings")
 django.setup()
 
-from channels.auth import AuthMiddlewareStack  # noqa E402
-from channels.routing import ProtocolTypeRouter, URLRouter  # noqa E402
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import OriginValidator
 from chat.routing import websocket_urlpatterns  # noqa E402
+
+# Define allowed origins for WebSocket connections
+ALLOWED_ORIGINS = [
+    "test.newsloom.io",  # Allow connections from this origin
+    "http://test.newsloom.io",  # Both HTTP and HTTPS should be allowed
+]
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+        # WebSocket with OriginValidator to restrict connections to allowed origins
+        "websocket": OriginValidator(
+            AuthMiddlewareStack(
+                URLRouter(websocket_urlpatterns)  # This will route WebSocket connections
+            ),
+            ALLOWED_ORIGINS,  # Validate that the WebSocket origin is allowed
+        ),
     }
 )
