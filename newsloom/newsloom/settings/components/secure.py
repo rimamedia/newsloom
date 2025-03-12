6650be24a1e6514d331e33f4f4a5fa681601303f
vadim <vadim@dcm-swiss.com>
2025-03-12 19:07:35 +0100

    strip fix

--- newsloom/newsloom/settings/components/secure.py
+++ newsloom/newsloom/settings/components/secure.py
@@ -2,7 +2,7 @@ from newsloom.contrib.env import get_env_var
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
-CSRF_TRUSTED_ORIGINS = [o.strip for o in get_env_var('CSRF_TRUSTED_ORIGINS',list[str], '') if o.strip()]
+CSRF_TRUSTED_ORIGINS = [o.strip() for o in get_env_var('CSRF_TRUSTED_ORIGINS',list[str], '') if o.strip()]
 
 CSRF_COOKIE_SECURE =  get_env_var("CSRF_COOKIE_SECURE", bool, True)
 CSRF_USE_SESSIONS = get_env_var("CSRF_USE_SESSIONS", bool, True)
